cf946af6988abe0115f23cda47c20f3c6098783a
EgorX2000 <ebkrivoshapkin@yandex.ru>
2026-05-25 17:13:28 +0300

    Maps -> real DB

--- src/main/java/jobrecommender/repository/JobRepository.java
+++ src/main/java/jobrecommender/repository/JobRepository.java
@@ -1,7 +1,6 @@
 package jobrecommender.repository;
 
 import jobrecommender.domain.Job;
-import jobrecommender.domain.User;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
