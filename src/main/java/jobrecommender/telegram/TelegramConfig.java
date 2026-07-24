ed644406186c35e2d8ee438e6a22b571223701e6
EgorX2000 <ebkrivoshapkin@yandex.ru>
2026-07-24 17:24:41 +0300

    Removed Telegram bot initialization from tests

--- src/main/java/jobrecommender/telegram/TelegramConfig.java
+++ src/main/java/jobrecommender/telegram/TelegramConfig.java
@@ -5,6 +5,7 @@ import jobrecommender.service.StringCommandsProcessingService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -12,6 +13,7 @@ import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
+@Profile("!test")
 public class TelegramConfig {
     private BotSession botSession;
 
